5f8c29d62c1a167b2272f043971f0dbdab180f10
Abimael Sergio <abimaelsergio@rivian.com>
2023-10-12 15:39:07 -0300

    Quick Sort: Ordenação pelo pivô

--- algoritmosII/aula3/encontrarMenores.js
+++ algoritmosII/aula3/encontrarMenores.js
@@ -19,5 +19,20 @@ function trocaLugar(array, de, para) {
     array[de] = elem2;
 }
 
-const array = encontraMenores(listaLivros[2], listaLivros);
+function divideNoPivo(array) {
+    let pivo = array[Math.floor(array.length / 2)];
+    encontraMenores(pivo, array);
+    let valoresMenores = 0;
+    for (let analisando = 0; analisando < array.length; analisando++) {
+        let atual = array[analisando];
+        if (atual.preco <= pivo.preco && atual !== pivo) {
+            trocaLugar(array, analisando, valoresMenores);
+            valoresMenores++;
+        }
+    }
+    return array;
+}
+
+//const array = encontraMenores(listaLivros[2], listaLivros);
+const array = divideNoPivo(listaLivros);
 console.log(array);
